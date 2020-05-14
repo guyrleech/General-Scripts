a1e35a776ef9f17ca5541d089493c42c2aedd4de
Guy Leech <guyrleech@gmail.com>
2020-05-14 21:20:58 +0100

    Marker functionality added
    Can sync with procmon by checking for existence of a marker file when Mark button pressed (or via keyboard via alt m)
    Asks for text for the marker, displays in list view and writes to pipeline on exit

--- Digital Clock.ps1
+++ Digital Clock.ps1
@@ -13,6 +13,10 @@ Run a stopwatch rather than showing current time
 
 Start the stopwatch immediately
 
+.PARAMETER markerFile
+
+A file to look for which will be then seen in a SysInternals Process Monitor trace as a CreateFile operation to allow cross referencing to that
+
 .PARAMETER timeoutSeconds
 
 The time in seconds to wait for the user interface to appear before declaring an error
@@ -41,6 +45,7 @@ Display a stopwatch in a window and start it immediately
 
     @guyrleech 14/05/2020  Initial release
                            Rewrote to use WPF DispatcherTimer rather than runspaces
+                           Added marker functionality
 #>
 
 [CmdletBinding()]
@@ -49,6 +54,7 @@ Param
 (
     [switch]$stopWatch ,
     [switch]$start ,
+    [string]$markerFile ,
     [int]$timeoutSeconds = 30
 )
 
@@ -63,11 +69,50 @@ Param
         xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
         xmlns:local="clr-namespace:Timer"
         mc:Ignorable="d"
-        Title="Guy's Clock" Height="225.194" Width="404.265">
+        Title="Guy's Clock" Height="272.694" Width="621.765">
     <Grid>
         <TextBox x:Name="txtClock" HorizontalAlignment="Left" Height="126" Margin="24,29,0,0" TextWrapping="Wrap" Text="TextBox" VerticalAlignment="Top" Width="358" FontSize="72" IsReadOnly="True" FontWeight="Bold" BorderThickness="0"/>
-        <CheckBox x:Name="checkboxRun" Content="Run" HorizontalAlignment="Left" Height="18" Margin="24,160,0,0" VerticalAlignment="Top" Width="151" IsChecked="True"/>
-        <Button x:Name="btnReset" Content="Reset" HorizontalAlignment="Left" Height="23" Margin="162,155,0,0" VerticalAlignment="Top" Width="147"/>
+        <Grid Margin="24,200,294,10">
+            <CheckBox x:Name="checkboxRun" Content="_Run" HorizontalAlignment="Left" Height="18" Margin="-11,5,0,0" VerticalAlignment="Top" Width="76" IsChecked="True"/>
+            <Button x:Name="btnReset" Content="Re_set" HorizontalAlignment="Left" Height="23" Margin="188,0,0,0" VerticalAlignment="Top" Width="95"/>
+            <Button x:Name="btnMark" Content="_Mark" HorizontalAlignment="Left" Height="23" Margin="65,0,0,0" VerticalAlignment="Top" Width="95"/>
+
+        </Grid>
+        <TextBox x:Name="txtMarkerFile" HorizontalAlignment="Left" Height="24" Margin="92,154,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="268"/>
+        <Label Content="Marker File" HorizontalAlignment="Left" Height="23" Margin="10,155,0,0" VerticalAlignment="Top" Width="72"/>
+        <ListView x:Name="listMarkings" HorizontalAlignment="Left" Height="210" Margin="375,13,0,0" VerticalAlignment="Top" Width="229" >
+            <ListView.View>
+                <GridView>
+                    <GridView.ColumnHeaderContextMenu>
+                        <ContextMenu/>
+                    </GridView.ColumnHeaderContextMenu>
+                    <GridViewColumn Header="Timestamp" DisplayMemberBinding="{Binding Timestamp}"/>
+                    <GridViewColumn Header="Notes" DisplayMemberBinding="{Binding Notes}"/>
+                </GridView>
+            </ListView.View>
+        </ListView>
+    </Grid>
+</Window>
+'@
+
+[string]$markerTextXAML = @'
+<Window x:Class="Timer.Test"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns:local="clr-namespace:Timer"
+        mc:Ignorable="d"
+        Title="Marker Text" Height="285.211" Width="589.034">
+    <Grid>
+        <Grid.ColumnDefinitions>
+            <ColumnDefinition Width="31*"/>
+            <ColumnDefinition Width="217*"/>
+            <ColumnDefinition Width="544*"/>
+        </Grid.ColumnDefinitions>
+        <TextBox x:Name="textBoxMarkerText" Grid.ColumnSpan="2" Grid.Column="1" HorizontalAlignment="Left" Height="97" Margin="0,31,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="533"/>
+        <Button x:Name="btnMarkerTextOk" Content="OK" Grid.Column="1" HorizontalAlignment="Left" Height="48" Margin="0,160,0,0" VerticalAlignment="Top" Width="120" IsDefault="True"/>
+        <Button x:Name="btnMarkerTextOk_Copy" Content="Cancel" Grid.Column="1" HorizontalAlignment="Left" Height="48" Margin="148,160,0,0" VerticalAlignment="Top" Width="120" Grid.ColumnSpan="2" IsCancel="True"/>
     </Grid>
 </Window>
 '@
@@ -136,6 +181,36 @@ $WPFbtnReset.Add_Click({
 
 $WPFbtnReset.IsEnabled = $stopWatch
 
+$WPFbtnMark.Add_Click({
+    $_.Handled = $true
+
+    [string]$timestamp = $(if( $stopWatch ) { '{0:d2}:{1:d2}:{2:d2}.{3:d3}' -f $timer.Elapsed.Hours , $timer.Elapsed.Minutes , $timer.Elapsed.Seconds, $timer.Elapsed.Milliseconds } else { Get-Date -Format 'HH:mm:ss.ffffff' } )
+    
+    ## if file exists then read else write it
+    if( ! [string]::IsNullOrEmpty( $WPFtxtMarkerFile.Text ) )
+    {
+        ## SysInternals Process Monitor will see this so can be cross referenced to here
+        Test-Path -Path (([Environment]::ExpandEnvironmentVariables( $WPFtxtMarkerFile.Text ))) -ErrorAction SilentlyContinue
+    }
+    ## add current time/stopwatch to gridview
+    if( $markerTextForm = Load-GUI -inputXaml $markerTextXAML )
+    {
+        $markerTextForm.TopMost = $true
+        $WPFbtnMarkerTextOk.Add_Click({
+            $_.Handled = $true          
+            $markerTextForm.DialogResult = $true 
+            $markerTextForm.Close()
+            })
+            
+        $WPFtextBoxMarkerText.Focus()
+
+        if( $markerTextForm.ShowDialog() )
+        {
+            $null = $WPFlistMarkings.Items.Add( ([pscustomobject]@{ 'Timestamp' = $timestamp ; 'Notes' = $WPFtextBoxMarkerText.Text.ToString() }) )
+        }
+    }
+})
+
 $WPFcheckboxRun.Add_Click({
     $_.Handled = $true
     if( $stopWatch )
@@ -187,11 +262,16 @@ $form.add_KeyDown({
 
 ## https://richardspowershellblog.wordpress.com/2011/07/07/a-powershell-clock/
 $form.Add_SourceInitialized({
-    $formTimer = New-Object -TypeName System.Windows.Threading.DispatcherTimer
-    $formTimer.Interval = $(if( $stopWatch ) { [Timespan]'00:00:00.100' } else { [Timespan]'00:00:00.750' })
-    $formTimer.Add_Tick( $timerBlock )
-    $formTimer.Start()
+    if( $formTimer = New-Object -TypeName System.Windows.Threading.DispatcherTimer )
+    {
+        ## need 0.1s granularity for the stopwatch but only sub-second for the clock
+        $formTimer.Interval = $(if( $stopWatch ) { [Timespan]'00:00:00.100' } else { [Timespan]'00:00:00.750' })
+        $formTimer.Add_Tick( $timerBlock )
+        $formTimer.Start()
+    }
 })
+
+$WPFtxtMarkerFile.Text = $markerFile
 
 $timer = New-Object -TypeName Diagnostics.Stopwatch
 
@@ -210,3 +290,9 @@ else
 }
 
 $null = $Form.ShowDialog()
+
+## put marker items onto the pipeline so can be copy'n'pasted into notes
+if( $WPFlistMarkings.Items -and $WPFlistMarkings.Items.Count )
+{
+    $WPFlistMarkings.Items
+}
